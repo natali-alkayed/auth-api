f63f55a53d1640e0d36560ca8ce7cb6664161208
natali-alkayed <nataly2000alkayed@gmail.com>
2023-06-25 16:37:23 +0000

    lab 8 done

---
+++ src/models/ index.js
@@ -0,0 +1,27 @@
+// 'use strict';
+
+// require('dotenv').config();
+// const { Sequelize, DataTypes } = require('sequelize');
+// const clothesModel = require('./ clothes/model');
+// const foodModel = require('./food/ model');
+// const Collection = require('./data-collections');
+
+// const DATABASE_URL = process.env.DATABASE_URL || 'sqlite:memory:';
+
+// const sequelize = new Sequelize(DATABASE_URL);
+// const food = foodModel(sequelize, DataTypes);
+// const clothes = clothesModel(sequelize, DataTypes);
+
+// module.exports = {
+//   db: sequelize,
+//   food: new Collection(food),
+//   clothes: new Collection(clothes),
+// };
+
+
+
+
+
+
+
+
